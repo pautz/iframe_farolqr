d9e838f2611d63896bd90dc5d678da5a4b66ded5
Carlito Pautz <37116894+pautz@users.noreply.github.com>
2026-06-19 20:13:34 -0400

    Update iframe source URL in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <div style="float: left; width: 230px; margin-right: 20px;">
-    <iframe src="https://farolqr.com/banner_lateral/banner_lateral.php" 
+    <iframe src="https://farolqr.com/banner_lateral/" 
             style="width: 100%; height: 650px; border: none; overflow: hidden;" 
             scrolling="no">
     </iframe>
