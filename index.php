b8462f15766552f2527afd7d5a4b31bc6273b732
Carlito Pautz <37116894+pautz@users.noreply.github.com>
2026-06-19 20:11:25 -0400

    Add iframe for lateral banner in index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<div style="float: left; width: 230px; margin-right: 20px;">
+    <iframe src="https://farolqr.com/banner_lateral/banner_lateral.php" 
+            style="width: 100%; height: 650px; border: none; overflow: hidden;" 
+            scrolling="no">
+    </iframe>
+</div>
